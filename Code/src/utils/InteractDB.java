34b9f2eef92042112f695c99181f6c724c05df93
gabriel-farache <gabriel.farache@gmail.com>
2013-11-11 13:42:51 +0100

    Refacto with more general methods

--- Code/src/utils/InteractDB.java
+++ Code/src/utils/InteractDB.java
@@ -19,112 +19,80 @@ public class InteractDB {
 		return InteractDB.connection;
 	}
 
-	public static int addTerm(String term) throws SQLException {
+	private static int doUpdate(String req) throws SQLException {
 		if (connection == null) {
 			return -1;
 		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO Terme (terme) " + "VALUES("+term+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
+		java.sql.PreparedStatement stmt = connection.prepareStatement(req, java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
 		return stmt.executeUpdate();
 	}
 	
-	public static int addType(String xpath, String type) throws SQLException {
-		if (connection == null) {
-			return -1;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO Types (xpath, terme) " + "VALUES("+xpath+", "+type+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
-		return stmt.executeUpdate();
-	}
-	
-	public static int addDocument(int numDoc, String author, String datePublication) throws SQLException {
-		if (connection == null) {
-			return -1;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO Documents (num_doc, auteur, datePublication) " + "VALUES("+numDoc+", "+author+", "+datePublication+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
-		return stmt.executeUpdate();
-	}
-	
-	public static int addContenirTerme(int idTerme, int idTypes, int freq) throws SQLException {
-		if (connection == null) {
-			return -1;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO ContenirTerme (idTerme, idTypes, frequence) " + "VALUES("+idTerme+", "+idTypes+", "+freq+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
-		return stmt.executeUpdate();
-	}
-	
-	public static int addSituer(int position, int idContenir) throws SQLException {
-		if (connection == null) {
-			return -1;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO Situer (position, idContenir) " + "VALUES("+position+", "+idContenir+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
-		return stmt.executeUpdate();
-	}
-	
-	public static int addContenirTypes(int idDoc, int idTypes) throws SQLException {
-		if (connection == null) {
-			return -1;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("INSERT INTO ContenirTypes (idDoc, idTypes) " + "VALUES("+idDoc+", "+idTypes+")", java.sql.ResultSet.TYPE_SCROLL_SENSITIVE, java.sql.ResultSet.CONCUR_UPDATABLE) ;
-		return stmt.executeUpdate();
-	}
-
-	public static ResultSet getTerm(int id) throws SQLException{
+	private static ResultSet doRequest(String req) throws SQLException {
 		if (connection == null) {
 			return null;
 		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT term FROM Term WHERE id="+id, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
+		java.sql.PreparedStatement stmt = connection.prepareStatement(req, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
 		return stmt.executeQuery();
+	}
+	
+	public static int addTerm(String term) throws SQLException {
+		return doUpdate("INSERT INTO Terme (terme) " + "VALUES("+term+")");
+	}
+	
+	public static int addType(String xpath, String type) throws SQLException {
+		return doUpdate("INSERT INTO Types (xpath, terme) " + "VALUES("+xpath+", "+type+")");
+	}
+	
+	public static int addDocument(int numDoc, String author, String datePublication) throws SQLException {
+		return doUpdate("INSERT INTO Documents (num_doc, auteur, datePublication) " + "VALUES("+numDoc+", "+author+", "+datePublication+")");
+	}
+	
+	public static int addContenirTerme(int idTerme, int idTypes, int freq) throws SQLException {
+		return doUpdate("INSERT INTO ContenirTerme (idTerme, idTypes, frequence) " + "VALUES("+idTerme+", "+idTypes+", "+freq+")");
+	}
+	
+	public static int addSituer(int position, int idContenir) throws SQLException {
+		return doUpdate("INSERT INTO Situer (position, idContenir) " + "VALUES("+position+", "+idContenir+")");
+	}
+	
+	public static int addContenirTypes(int idDoc, int idTypes) throws SQLException {
+		return doUpdate("INSERT INTO ContenirTypes (idDoc, idTypes) " + "VALUES("+idDoc+", "+idTypes+")");
+	}
+
+	public static ResultSet getTerm(int id) throws SQLException{
+		return doRequest("SELECT term FROM Term WHERE id="+id);
 		
 	}
 	
 	public static ResultSet getTermIDs(String term) throws SQLException{
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT id FROM Term WHERE term='"+term+"'", java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery();
+		return doRequest("SELECT id FROM Term WHERE term='"+term+"'");
 		
 	}
 	
 	public static ResultSet getDocByNum(int numDoc) throws SQLException{
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Documents WHERE num_doc="+numDoc, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery();
+		return doRequest("SELECT * FROM Documents WHERE num_doc="+numDoc);
 		
 	}
 	
 	public static Integer getFreqTermType(int idTerm, int idType) throws SQLException{
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT frequence FROM ContenirTerme WHERE idTerm="+idTerm+" AND idType="+idType, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery().getInt("frequence");
+		ResultSet res =  doRequest("SELECT frequence FROM ContenirTerme WHERE idTerm="+idTerm+" AND idType="+idType) ;
+		return res == null ? null : res.getInt("frequence");
 		
 	}
 	
 	public static Integer getIDContenirTerm(int idTerm, int idType) throws SQLException{
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT id FROM ContenirTerme WHERE idTerm="+idTerm+" AND idType="+idType, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery().getInt("id");
+		ResultSet res =  doRequest("SELECT id FROM ContenirTerme WHERE idTerm="+idTerm+" AND idType="+idType) ;
+		return res == null ? null : res.getInt("id");
 		
 	}
 	
 	public static Boolean containsTypeInDoc(int idType, int idDoc) throws SQLException {
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT id FROM ContenirTypes WHERE idType="+idType+" AND idDoc="+idDoc, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery().first();
+		ResultSet res =  doRequest("SELECT id FROM ContenirTypes WHERE idType="+idType+" AND idDoc="+idDoc) ;
+		return res == null ? null : res.first();
 	}
 	
 	public static Integer getPositionTerm(int idContenir) throws SQLException {
-		if (connection == null) {
-			return null;
-		}
-		java.sql.PreparedStatement stmt = connection.prepareStatement("SELECT position FROM Situer WHERE idContenir="+idContenir, java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE, java.sql.ResultSet.CONCUR_READ_ONLY ) ;
-		return stmt.executeQuery().getInt("position");
+		ResultSet res =  doRequest("SELECT position FROM Situer WHERE idContenir="+idContenir) ;
+		return res == null ? null : res.getInt("position");
 	}
 }
